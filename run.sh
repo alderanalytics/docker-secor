3937951be0d0b60e4817d4bd7f2f1ecc66589332
Alfred Rossi <alfredr@gmail.com>
2015-10-20 18:48:40 -0400

    change options

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-SECOR_JAR=$(ls secor-*-SNAPSHOT.jar)
+SECOR_JAR_GUESS=$(ls secor-*-SNAPSHOT.jar)
+
+SECOR_JAR=${SECOR_JAR:-$SECOR_JAR_GUESS}
+SECOR_BASE_CONFIG=${SECOR_BASE_CONFIG:-secor.prod.backup.properties}
+SECOR_MESSAGE_PARSER=${SECOR_MESSAGE_PARSER:-com.pinterest.secor.parser.OffsetMessageParser}
+SECOR_GROUP=${SECOR_GROUP:-secor_backup}
 
 exec java -ea -Dsecor.kafka.group=${SECOR_GROUP} \
-    -Daws.access.key=${AWS_ACCESS_KEY} \
-    -Daws.secret.key=${AWS_SECRET_KEY} \
-    -Dzookeeper.quorum=${ZK_QUORUM} \
-    -Dkafka.seed.broker.host=${SEED_BROKER_HOST} \
-    -Dsecor.s3.bucket=${S3_BUCKET} \
-    -Dsecor.s3.path=${S3_PATH} \
+    -Daws.access.key=${SECOR_AWS_ACCESS_KEY} \
+    -Daws.secret.key=${SECOR_AWS_SECRET_KEY} \
+    -Dzookeeper.quorum=${SECOR_ZK_CLUSTER} \
+    -Dkafka.seed.broker.host=${SECOR_KAFKA_HOST} \
+    -Dsecor.s3.bucket=${SECOR_S3_BUCKET} \
+    -Dsecor.s3.path=${SECOR_S3_PATH} \
+    -Dsecor.message.parser.class=${SECOR_MESSAGE_PARSER} \
     -Dlog4j.configuration=log4j.prod.properties \
-    -Dconfig=secor.prod.backup.properties \
+    -Dconfig=${SECOR_BASE_CONFIG} \
     -cp ${SECOR_JAR}:lib/* \
     com.pinterest.secor.main.ConsumerMain
